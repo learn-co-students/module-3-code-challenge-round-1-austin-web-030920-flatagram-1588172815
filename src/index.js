21ac09be858989fe575459689d13fb8446236b31
github username <github email address>
2020-04-29 12:13:00 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // write your code here
+
+
+
+const url = "http://localhost:3000/image"
+
+// const imageDiv = document.querySelector('image-card');
+// const likeDiv = document.querySelector('likes-section');
+// const title = document.querySelector('title');
+// const commentDiv = document.querySelector('comments');
+// const commentSubmit = document.querySelector('comment-form');
+let pic;
+
+document.addEventListener  ('DOMContentLoaded', () =>{
+    fetchPic();
+})
+
+function fetchPic(){
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        pic = json;
+        renderPic(json)
+    })
+}
+
+
+function renderPic(json){
+    console.log(json)
+
+    let title = document.getElementById('title');
+    title.innerHtml = pic.title;
+
+    // let image = document.getElementById('image');
+    // image.src = pic.image;
+
+
+
+}
